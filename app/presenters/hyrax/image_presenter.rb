a6d2e6a24f54ea04a583b60e87783fbb91886627
JocelynTriplett <jocelyn.triplett@gmail.com>
2018-09-27 12:10:43 -0400

    fix bad presenter edits

--- app/presenters/hyrax/image_presenter.rb
+++ app/presenters/hyrax/image_presenter.rb
@@ -3,12 +3,5 @@
 module Hyrax
   class ImagePresenter < Hyrax::WorkShowPresenter
     delegate :year, to: :solr_document
-
-    def initialize(image)
-      @image = image
-    end
-
-    delegate :member_of, to: :@image
-
   end
 end
